331604723ec30c8e28194c82dc5493ed78560ef9
Elizabeth Doyle <lizdoyle1121@gmail.com>
2019-12-08 23:20:50 -0500

    cmplt mvp, turned off extra routes, and cmplt tests

--- api/api-router.js
+++ api/api-router.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 
-const loginRouter = require("../login/login-router.js");
+//const loginRouter = require("../login/login-router.js");
 const usersRouter = require("../users/users-router.js");
-const registerRouter = require("../register/register-router.js");
+//const registerRouter = require("../register/register-router.js");
 
 
-router.use("/login", loginRouter);
+//router.use("/login", loginRouter);
 router.use("/users", usersRouter);
-router.use("/register", registerRouter);
+//router.use("/register", registerRouter);
 
 
 router.get("/", (req, res) => {
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
-const apiRouter = require("./api-router.js");
-const configureMiddleware = require("./configure-middleware.js");
+const apiRouter = require("./api/api-router.js");
+const configureMiddleware = require("./api/configure-middleware.js");
 
 const server = express();
 
