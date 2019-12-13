73eba7c46b01fe76ace5d841caa57d162ada9a80
Elizabeth Doyle <lizdoyle1121@gmail.com>
2019-12-12 21:40:42 -0500

    addinjg server config

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require("express");
+const session = require('express-session');
 
 const apiRouter = require("./api/api-router.js");
 const configureMiddleware = require("./api/configure-middleware.js");
 
 const server = express();
 
+const sessionConfig = {
+        name: 'sunglasses',
+        secret: "ilovesecretstheyarefunandoverlycomplicated",
+
+        cookie: {
+            maxAge: 1000 * 30,
+            secure: false,
+            httpOnly: true,
+        }
+
+        resave: false,
+        saveUninitialized: false,
+}
+
 configureMiddleware(server);
 
+server.use(sesseion(sessionConfig))
+
 server.use("/api", apiRouter);
+
+server.get('/', (req, res) => {
+    res.json({api: "This is working"})
+});
 
 module.exports = server;
